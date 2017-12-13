4d2361ba078776f1d536a0a27eef0f84a1d02ce6
DELL <gzd4215@163.com>
2017-12-13 09:03:04 +0800

    外星人入侵

--- blog_spider/blog_analyze.py
+++ blog_spider/blog_analyze.py
@@ -5,7 +5,7 @@ import pandas as pd
 import pymysql
 from pandas import DataFrame as df
 
-conn = pymysql.connect(host='101.200.83.54', port=3306, user='root', passwd='Password!1', db='blog_log')
+conn = pymysql.connect(host='XXXssssXX', port=3306, user='root', passwd='Password', db='blog_log')
 conn.set_charset("utf8")
 sql_result = pd.read_sql('select * from t_read_num t ', con=conn)
 print(sql_result)
