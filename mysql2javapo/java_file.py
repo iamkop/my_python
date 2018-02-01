d27355261bb4eac536eaf3bb6ed5a406d5571eec
DELL <gzd4215@163.com>
2018-02-01 11:21:36 +0800

    增加get,set方法

--- mysql2javapo/java_file.py
+++ mysql2javapo/java_file.py
@@ -3,6 +3,7 @@
 import mysql2javapo.constant as constant
 import datetime
 
+
 def _build_blank_line():
     """ 写空格行 """
     return '\n'
@@ -24,27 +25,70 @@ def build_po(package_name, columns):
 
 
 class JavaFileWriter:
-    def __init__(self, path, encoding, package_name, columns):
+    def __init__(self, encoding, package_name, columns):
         """ 列格式: [[name1,type1,comment2],[name2 ...]] """
+        self.encoding = encoding
         self.columns = columns
-        self.package_info = 'package {package_name};\n\n'.format(package_name=package_name)
-        self.import_info = ''
-        self.class_doc = '/**\n * @author userName\n * @date {time}' \
-                         '\n * @Description todo(用一句话描述该文件做什么)\n */'\
+        self.package_info = 'package {package_name};'.format(package_name=package_name)
+        self.import_info = []
+        self.class_doc = '\n\n/**\n * @author ${{userName}}\n * @date {time}' \
+                         '\n * @Description ${{todo}}(用一句话描述该文件做什么)\n */\n' \
             .format(time=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                                                                       )
-        self.class_structure = '{0}{1}{2}public class Test {{\n{3}\n}}'
-        # with open(path, 'w', encoding=encoding) as w:
-        #     w.write(build_po(package_name, columns))
+                    )
+        self.class_structure = '{0}{1}{2}public class Test {{{3}{4}\n}}'
 
-    def build_class(self):
-        """ 构建类 """
-        print(self.class_doc)
+    def build_field_import(self):
+        """ 构建类的字段 """
+        fields = []
+        get_set = []
+        # 循环处理
         for column in self.columns:
-            # print(column)
-            pass
+            column_name = column[0]
+            column_type = column[1]
+            column_common = column[2]
+            java_type = constant.mysql_2_java_type[column_type]
+            retract = constant.retract
+            # 是否需要import java 包,如果需要
+            if isinstance(java_type, list):
+                java_type_real = java_type[0]
+                import_this = 'import ' + java_type[1] + ';'
+                if import_this not in self.import_info:
+                    self.import_info.append('import ' + java_type[1] + ';')
+            # 如果不需要
+            else:
+                java_type_real = java_type
+            # 下划线分隔变为驼峰写法
+            column_split = column_name.split('_')
+            column_final_name = ''
+            for column_script in column_split:
+                # 第一个单词不首字母大写
+                if column_split.index(column_script) > 0:
+                    column_final_name += column_script.capitalize()
+                else:
+                    column_final_name += column_script
+            fields.append('\n\n' + retract + '/** {common} */\n'.format(common=column_common) \
+                          + retract + 'private ' + java_type_real + ' ' + column_final_name + ';')
+            # 方法名需要首字母大写(紧跟get or set 后的首字母)
+            column_for_get_set = ''.join(map(lambda x: x.capitalize(), column_name.split('_')))
+            # get 方法的模板
+            get_temp = '\n\n    public {field_type} get{field_name_ca}() {{\n        return {field_name};\n    }}'
+            # set 方法的模板
+            set_temp = '\n\n    public void set{field_name_ca}({java_type_real} {field_name})' \
+                       ' {{\n        this.{field_name} = {field_name};\n    }}'
+            get_set.append(get_temp.format(field_type=java_type_real, field_name_ca=column_for_get_set
+                                           , field_name=column_final_name))
+            get_set.append(set_temp.format(field_name_ca=column_for_get_set, field_name=column_final_name
+                                           ,java_type_real=java_type_real))
+        return {'fields': fields, 'get_set': get_set}
 
-    # @staticmethod
-    # def write_class():
-    #     print("---")
-    #     pass
+    def write_class(self, path):
+        """ 写类文件 """
+        field_list = self.build_field_import()
+        fields = ''.join(field_list['fields'])
+        get_set = ''.join(field_list['get_set'])
+        imports = ''
+        if len(self.import_info) > 0:
+            imports = '\n\n' + '\n'.join(self.import_info)
+
+        with open(path, 'w', encoding=self.encoding) as w:
+            w.write(self.class_structure.format(self.package_info, imports, self.class_doc, fields, get_set))
--- mysql2javapo/main.py
+++ mysql2javapo/main.py
@@ -3,30 +3,30 @@
 from mysql2javapo.java_file import JavaFileWriter
 from mysql2javapo.mysql_executor import MysqlExecutor
 
-host = 'address'
+host = 'xxx'
 port = 3306
 user = 'root'
-passwd = 'password'
-db = 'dbName'
-get_columns_sql = 'SHOW FULL COLUMNS FROM test.sssss'
+passwd = 'xxx'
+db = 'xxx'
+get_columns_sql = 'SHOW FULL COLUMNS FROM xxx.xxx'
 package_name = 'com.gzd.po'
 path = 'C:\\Users\\DELL\\Desktop\\Test.java'
+
 
 def main():
     mysql_executor = MysqlExecutor(host=host, port=port, user=user, passwd=passwd, db=db)
     create_sql = mysql_executor.get_columns(get_columns_sql)
     mysql_executor.close()
-    print(create_sql)
     # 角标0:列名,1:数据类型,8:注释
     # 生成的结构[[name1,type1,comment2],[name2 ...]]
     columns = []
     for column in create_sql:
         columns_detail = [column[0], column[1].split('(')[0], column[8]]
         columns.append(columns_detail)
-    print(columns)
 
-    writer = JavaFileWriter(path, 'utf-8', package_name, columns)
-    writer.build_class()
+    writer = JavaFileWriter('utf-8', package_name, columns)
+    # writer.build_field()
+    writer.write_class(path)
 
 
 main()
